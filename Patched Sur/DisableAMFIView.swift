4aa8c280ee42b02f21cf009bfaddfd650c5b1d92
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-02-21 17:16:18 -0700

    Fix the AMFI Disabler from giving you the prohibited symbol

--- Patched Sur/DisableAMFIView.swift
+++ Patched Sur/DisableAMFIView.swift
@@ -17,8 +17,8 @@ struct DisableAMFIView: View {
                 .multilineTextAlignment(.center)
             EnterPasswordButton(password: $password) {
                 do {
-                    var current = (try? call("nvram boot-args")) ?? "boot-args    -no_compat_check"
-                    current.removeFirst("boot-args    ".count)
+                    var current = (try? call("nvram boot-args")) ?? "boot-args -no_compat_check"
+                    current.removeFirst("boot-args ".count)
                     try call("nvram boot-args=\"\(current) amfi_get_out_of_my_way=1\"", p: password)
                 } catch {
                     print("Failed to update NVRAM.")
