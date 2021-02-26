ee08f715114bfb95d22a84f6490a6fe19877b9d6
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-02-25 23:02:12 -0700

    More work on daemon Now I just need more excuses to write the word daemon

--- Patched Sur/PatchDaemon.swift
+++ Patched Sur/PatchDaemon.swift
@@ -17,20 +17,51 @@ func patchDaemon() {
         print("Checking for Patched Sur updates first...")
         if let patcherVersions = try? PatchedVersions(fromURL: "https://api.github.com/repos/BenSova/Patched-Sur/releases").filter({ !$0.prerelease }) {
             print("Checking if we already checked for this...")
-            if UserDefaults.standard.string(forKey: "LastCheckedVersion") != patcherVersions[0].tagName {
+            if UserDefaults.standard.string(forKey: "LastCheckedPSVersion") != patcherVersions[0].tagName {
                 print("Checking if we have a different version...")
                 if patcherVersions[0].tagName != "v\(AppInfo.version)" {
                     print("Sending update notification!")
                     var tagName = patcherVersions[0].tagName
                     tagName.removeFirst()
                     scheduleNotification(title: "Patched Sur \(patcherVersions[0].tagName)", body: "Patched Sur can now be updated to Version \(tagName). Open Patched Sur then click Update macOS to learn more.")
+                    print("Now making sure we don't sent this again...")
+                    UserDefaults.standard.setValue(patcherVersions[0].tagName, forKey: "LastCheckedPSVersion")
                 } else { print("Nope, we're up-to-date.") }
             } else { print("We already have sent this notification.") }
         } else {
             print("Failed to fetch them. oh well...")
         }
-        print("Now checking for macOS updates...")
-        
+        print("Getting ready to check for macOS updates")
+        print("Figuring out what update track to use...")
+        let track = ReleaseTrack(rawValue: UserDefaults.standard.string(forKey: "UpdateTrack") ?? "Release") ?? .release
+        print("Using update track \(track).")
+        print("Checking for macOS updates...")
+        if var macOSversions = try? InstallAssistants(fromURL: URL(string: "https://bensova.github.io/patched-sur/installers/\(track == .developer ? "Developer" : (track == .publicbeta ? "Public" : "Release")).json")!) {
+            print("Sorting the macOS versions...")
+            macOSversions = macOSversions.sorted { (first, second) -> Bool in
+                first.orderNumber > second.orderNumber
+            }
+            if UserDefaults.standard.string(forKey: "LastCheckedOSVersion") != macOSversions[0].buildNumber {
+                print("Checking our build number to compare...")
+                if let buildNumber = (try? call("sw_vers | grep BuildVersion:", at: ".")) {
+                    print("Checking if we have a different build number...")
+                    if buildNumber != macOSversions[0].buildNumber {
+                        print("Sending update notification!")
+                        scheduleNotification(title: "Software Update", body: "macOS Big Sur \(macOSversions[0].version) (\(macOSversions[0].buildNumber)) can now be installed on your device. Open Patched Sur then click Update macOS to learn more.")
+                        print("Now making sure we don't sent this again...")
+                        UserDefaults.standard.setValue(macOSversions[0].buildNumber, forKey: "LastCheckedOSVersion")
+                    } else {
+                        print("We're already on the latest version.")
+                    }
+                } else {
+                    print("We somehow failed to get the build number")
+                }
+            } else {
+                print("We've already sent this notification")
+            }
+        } else {
+            print("Failed to fetch them. oh well...")
+        }
         sleep(100000000)
     }
     
