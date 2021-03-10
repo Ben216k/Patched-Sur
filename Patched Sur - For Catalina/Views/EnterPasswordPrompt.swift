d86d61139afb8871d0688574fa50304185445ec2
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-03-09 20:38:13 -0700

    (Both-UI) The Continue button in the password prompt now responds to enter

--- Patched Sur - For Catalina/Views/EnterPasswordPrompt.swift
+++ Patched Sur - For Catalina/Views/EnterPasswordPrompt.swift
@@ -56,10 +56,10 @@ struct EnterPasswordPrompt: View {
                         }.padding(.bottom, 12)
                         HStack {
                             Spacer()
-                            Button("Cancel") {
+                            NativeButton("Continue", keyEquivalent: .none) {
                                 
                             }
-                            Button("Continue") {
+                            NativeButton("Continue", keyEquivalent: .return) {
                                 
                             }
                         }
@@ -69,6 +69,100 @@ struct EnterPasswordPrompt: View {
             }
             .frame(width: 425, height: 200)
             .cornerRadius(10)
+        }
+    }
+}
+
+// MARK: - Action closure for controls
+
+private var controlActionClosureProtocolAssociatedObjectKey: UInt8 = 0
+
+protocol ControlActionClosureProtocol: NSObjectProtocol {
+    var target: AnyObject? { get set }
+    var action: Selector? { get set }
+}
+
+private final class ActionTrampoline<T>: NSObject {
+    let action: (T) -> Void
+
+    init(action: @escaping (T) -> Void) {
+        self.action = action
+    }
+
+    @objc
+    func action(sender: AnyObject) {
+        action(sender as! T)
+    }
+}
+
+extension ControlActionClosureProtocol {
+    func onAction(_ action: @escaping (Self) -> Void) {
+        let trampoline = ActionTrampoline(action: action)
+        self.target = trampoline
+        self.action = #selector(ActionTrampoline<Self>.action(sender:))
+        objc_setAssociatedObject(self, &controlActionClosureProtocolAssociatedObjectKey, trampoline, .OBJC_ASSOCIATION_RETAIN)
+    }
+}
+
+extension NSControl: ControlActionClosureProtocol {}
+
+// MARK: -
+
+
+
+@available(macOS 10.15, *)
+struct NativeButton: NSViewRepresentable {
+    enum KeyEquivalent: String {
+        case escape = "\u{1b}"
+        case `return` = "\r"
+    }
+
+    var title: String?
+    var attributedTitle: NSAttributedString?
+    var keyEquivalent: KeyEquivalent?
+    let action: () -> Void
+
+    init(
+        _ title: String,
+        keyEquivalent: KeyEquivalent? = nil,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.keyEquivalent = keyEquivalent
+        self.action = action
+    }
+
+    init(
+        _ attributedTitle: NSAttributedString,
+        keyEquivalent: KeyEquivalent? = nil,
+        action: @escaping () -> Void
+    ) {
+        self.attributedTitle = attributedTitle
+        self.keyEquivalent = keyEquivalent
+        self.action = action
+    }
+
+    func makeNSView(context: NSViewRepresentableContext<Self>) -> NSButton {
+        let button = NSButton(title: "", target: nil, action: nil)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        button.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return button
+    }
+
+    func updateNSView(_ nsView: NSButton, context: NSViewRepresentableContext<Self>) {
+        if attributedTitle == nil {
+            nsView.title = title ?? ""
+        }
+
+        if title == nil {
+            nsView.attributedTitle = attributedTitle ?? NSAttributedString(string: "")
+        }
+
+        nsView.keyEquivalent = keyEquivalent?.rawValue ?? ""
+
+        nsView.onAction { _ in
+            self.action()
         }
     }
 }
