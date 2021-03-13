e947e45d745097cf7c9336cfa545d4a271ec8875
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-03-12 18:18:43 -0700

    Fix typo I guess

--- Patched Sur - For Catalina/Views/MainView.swift
+++ Patched Sur - For Catalina/Views/MainView.swift
@@ -15,7 +15,7 @@ struct MainView : View {
     var body: some View {
         VStack() {
             Text("Welcome to Patched Sur!").bold()
-            Text("This patcher is made to provide you with the simplest upgrade to macOS Big Sur on your unsupported Mac. It prepares everything to be perfect by the end of the installation, and makes sure that nothing will go wrong with your installtion. By the end, Patched Sur will try to make your Mac run just like a supported one.")
+            Text("This patcher is made to provide you with the simplest upgrade to macOS Big Sur on your unsupported Mac. It prepares everything to be perfect by the end of the installation, and makes sure that nothing will go wrong with your installation. By the end, Patched Sur will try to make your Mac run just like a supported one.")
                 .multilineTextAlignment(.center)
                 .padding()
             VIButton(id: "START", h: $hovered) {
