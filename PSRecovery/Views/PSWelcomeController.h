07a0c9073081b00c8c762d2d65cc2a886509ea33
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-04-15 22:10:47 -0600

    Limitations set by recovery mode are sad.

---
+++ PSRecovery/Views/PSWelcomeController.h
@@ -0,0 +1,12 @@
+//
+//  PSWelcomeController.h
+//  Patched Sur
+//
+//  Created by Ben Sova on 4/14/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PSWelcomeController : NSViewController
+
+@end
---
+++ PSRecovery/Views/VIButton.h
@@ -0,0 +1,14 @@
+//
+//  VIButton.h
+//  Patched Sur
+//
+//  Created by Ben Sova on 4/15/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VIButton : NSButton
+
+- (void)setButtonText:(NSString *)buttonText;
+
+@end
