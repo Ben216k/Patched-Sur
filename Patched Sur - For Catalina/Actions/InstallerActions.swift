8a1d956227c599e76550a548e402916c6ad5b068
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-04-25 13:52:34 -0600

    Way better handling of app installers and beta installers

--- Patched Sur - For Catalina/Actions/InstallerActions.swift
+++ Patched Sur - For Catalina/Actions/InstallerActions.swift
@@ -9,6 +9,8 @@ import Foundation
 
 // MARK: Extract/Move Installer
 
+var useBeta = false
+
 func extractPackage(installInfo: InstallAssistant, password: String, errorX: (String) -> (), beta: (String) -> ()) {
     do {
         if installInfo.buildNumber == "CustomPKG" {
@@ -16,18 +18,25 @@ func extractPackage(installInfo: InstallAssistant, password: String, errorX: (St
             _ = try? call("rm -rf '/Applications/Install macOS Big Sur.app'", p: password)
             _ = try? call("rm -rf '/Applications/Install macOS Big Sur Beta.app'", p: password)
             _ = try? call("rm -rf /Applications/PSInstaller.app", p: password)
+            print("Checking before-hand SHA Sums")
+            let betaSHA = try? call("shasum -a 1 /Applications/Install\\ macOS\\ Big\\ Sur\\ Beta.app/Contents/MacOS/InstallAssistant | cut -d \" \" -f 1")
             print("Extracting Package at \(installInfo.url)")
             try call("installer -pkg '\(installInfo.url)' -target /", p: password)
-            print("Done extracting package.")
-        } else {
-            if installInfo.url != "/Applications/Install macOS Big Sur.app" {
-                print("Cleaning up before move.")
-                _ = try? call("rm -rf '/Applications/Install macOS Big Sur.app'", p: password)
-                print("Moving installer app")
-                try call("cp -a '\(installInfo.url)' '/Applications/Install macOS Big Sur.app'", p: password)
-                print("Done moving this app for this dumb workaround that I have to use for dumb reasons.")
+            print("Checking back at SHA Sums")
+            let betaSHANew = try? call("shasum -a 1 /Applications/Install\\ macOS\\ Big\\ Sur\\ Beta.app/Contents/MacOS/InstallAssistant | cut -d \" \" -f 1")
+            if betaSHA != betaSHANew {
+                useBeta = true
             }
         }
+//        } else {
+//            if installInfo.url != "/Applications/Install macOS Big Sur.app" {
+//                print("Cleaning up before move.")
+//                _ = try? call("rm -rf '/Applications/Install macOS Big Sur.app'", p: password)
+//                print("Moving installer app")
+//                try call("cp -a '\(installInfo.url)' '/Applications/Install macOS Big Sur.app'", p: password)
+//                print("Done moving this app for this dumb workaround that I have to use for dumb reasons.")
+//            }
+//        }
         errorX("CREATE")
     } catch {
         print("Failed to extract package.")
@@ -66,7 +75,11 @@ func createInstallMedia(volume: String, installInfo: InstallAssistant, password:
         _ = try? call("sleep 2")
         print("Starting createinstallmedia")
         progressText("Starting createinstallmedia")
-        try call("/Applications/Install\\ macOS\\ Big\\ Sur*/Contents/Resources/createinstallmedia --volume '/Volumes/\(volume)' --nointeraction", p: password, h: progressText)
+        if installInfo.buildNumber.contains("APP") {
+            try call("\(installInfo.url)/Contents/Resources/createinstallmedia --volume '/Volumes/\(volume)' --nointeraction", p: password, h: progressText)
+        } else {
+            try call("/Applications/Install\\ macOS\\ Big\\ Sur\(useBeta ? "\\ Beta" : "").app/Contents/Resources/createinstallmedia --volume '/Volumes/\(volume)' --nointeraction", p: password, h: progressText)
+        }
         print("Finished createinstallmedia!")
         errorX("PATCH")
     } catch {
@@ -100,8 +113,11 @@ func patchInstaller(password: String, progressText: @escaping (String) -> (), er
         _ = try? call("killall mds", p: password)
         _ = try? call("killall mds_stores", p: password)
         print("Starting to patch USB installer")
+        #if !DEBUG
         try call("/usr/local/lib/Patched-Sur-Patches/Scripts/PatchUSB.sh '\(Bundle.main.resourcePath!)'", p: password, h: progressText)
-//        try call("/Users/bensova/Developer/Patched-Sur-Patches/Scripts/PatchUSB.sh '\(Bundle.main.resourcePath!)'", p: password, h: progressText)
+        #else
+        try call("/Users/bensova/Developer/Patched-Sur-Patches/Scripts/PatchUSB.sh '\(Bundle.main.resourcePath!)'", p: password, h: progressText)
+        #endif
         progressText("Copying Post-Install App")
         print("Copying Post-Install App")
         if let bundlePath = Bundle.main.path(forResource: "Patched Sur.app", ofType: nil) {
